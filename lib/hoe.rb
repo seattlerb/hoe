0cd8f47b1a6e066c89051106ce0696cfa7ec93ef
Ryan Davis <ryand@zenspider.com>
2009-03-30 16:48:40 -0800

    + Added from vlad: Rake.application.all_tasks and Rake.clear_tasks. + Added Rake.undo. [git-p4: depot-paths = "//src/hoe/dev/": change = 4807]

--- lib/hoe.rb
+++ lib/hoe.rb
@@ -1158,6 +1158,46 @@ class Hoe
   end
 end
 
+module Rake
+  module TaskManager
+    ##
+    # This gives us access to the tasks already defined in rake.
+    def all_tasks
+      @tasks
+    end
+  end
+
+  ##
+  # Hooks into rake and allows us to clear out a task by name or
+  # regexp. Use this if you want to completely override a task instead
+  # of extend it.
+  def self.clear_tasks(*tasks)
+    tasks.flatten.each do |name|
+      case name
+      when Regexp then
+        Rake.application.all_tasks.delete_if { |k,_| k =~ name }
+      else
+        Rake.application.all_tasks.delete(name)
+      end
+    end
+  end
+
+  ##
+  # Removes the last action added to a task. Use this when two
+  # libraries define the same task and you only want one of the
+  # actions.
+  #
+  #   require 'hoe'
+  #   require 'tasks/rails'
+  #   Rake.undo("test") # rolls out rails' test task
+
+  def self.undo(*names)
+    names.each do |name|
+      all_tasks[name].actions.delete_at(-1)
+    end
+  end
+end
+
 # :enddoc:
 
 class ::Rake::SshDirPublisher # :nodoc:
