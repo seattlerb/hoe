e814a32177414bb73c31bd7c44b096517bdd82fc
Ryan Davis <ryand@zenspider.com>
2009-06-30 20:10:48 -0800

    prepping for release [git-p4: depot-paths = "//src/hoe/dev/": change = 5281]

--- lib/hoe.rb
+++ lib/hoe.rb
@@ -59,7 +59,7 @@ require 'hoe/rake'
 
 class Hoe
   # duh
-  VERSION = '2.3.1'
+  VERSION = '2.3.2'
 
   @@plugins = [:clean, :debug, :deps, :flay, :flog, :package, :publish,
                :rcov, :signing, :test]
