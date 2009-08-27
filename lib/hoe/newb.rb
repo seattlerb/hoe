03cad00a4ae78a4176b7e8e1e5c3b78d139a2c99
Ryan Davis <ryand@zenspider.com>
2009-08-27 13:47:51 -0800

    john sucks [git-p4: depot-paths = "//src/hoe/dev/": change = 5435]

--- lib/hoe/newb.rb
+++ lib/hoe/newb.rb
@@ -6,6 +6,7 @@
 # newb:: Get a new developer up to speed.
 
 module Hoe::Newb
+  # define tasks for the newb plugin
   def define_newb_tasks
     desc "Install deps, generate docs, run tests/specs."
     task :newb => %w(check_extra_deps docs default) do
