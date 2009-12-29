189f2d5bfb22ab75450a752f8eeda900e9c8a025
Ryan Davis <ryand@zenspider.com>
2009-12-29 14:13:25 -0800

    Clean up namespacing [git-p4: depot-paths = "//src/hoe/dev/": change = 5572]

--- lib/hoe/gemcutter.rb
+++ lib/hoe/gemcutter.rb
@@ -1,20 +1,18 @@
 require 'rake'
 
-class Hoe
-  module Gemcutter
-    def define_gemcutter_tasks
-      desc "Push gem to gemcutter."
-      task :release_to_gemcutter => [:clean, :package, :release_sanity] do
-        pkg   = "pkg/#{spec.name}-#{spec.version}"
-        gems  = Dir["#{pkg}*.gem"]
-        gems.each do |g|
-          # TODO - once gemcutter supports command invocation, use it.
-          # We could still fail here due to --format executable
-          sh Gem.ruby, "-S", "gem", "push", g
-        end
+module Hoe::Gemcutter
+  def define_gemcutter_tasks
+    desc "Push gem to gemcutter."
+    task :release_to_gemcutter => [:clean, :package, :release_sanity] do
+      pkg   = "pkg/#{spec.name}-#{spec.version}"
+      gems  = Dir["#{pkg}*.gem"]
+      gems.each do |g|
+        # TODO - once gemcutter supports command invocation, use it.
+        # We could still fail here due to --format executable
+        sh Gem.ruby, "-S", "gem", "push", g
       end
-
-      task :release_to => :release_to_gemcutter
     end
-  end unless defined? Gemcutter # HACK - tests force load twice
+
+    task :release_to => :release_to_gemcutter
+  end unless public_instance_methods.include? "define_gemcutter_tasks"
 end
