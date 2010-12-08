d37e32a2254457dd40a1113781678ca501fc621e
Ryan Davis <ryand@zenspider.com>
2010-12-07 20:24:41 -0800

    Added doco and parameterized the compile tasks [git-p4: depot-paths = "//src/hoe/dev/": change = 6042]

--- lib/hoe/compiler.rb
+++ lib/hoe/compiler.rb
@@ -1,11 +1,30 @@
 ##
 # rake-compiler plugin for hoe c-extensions.
+#
+# This plugin is for extconf.rb based projects that want to use
+# rake-compiler to deal with packaging binary gems. It expects a
+# standard extconf setup, namely that your extconf.rb and c source is
+# located in: ext/project-name.
+#
+# === Tasks Provided:
+#
+# compile::     Compile your c-extension.
 
 module Hoe::Compiler
-  def initialize_compiler
-    extra_dev_deps << ["rake-compiler", "~> 0.7"]
 
-    self.spec_extras = { :extensions => ["ext/#{self.name}/extconf.rb"] }
+  ##
+  # Optional: Defines what tasks need to be compile first. [default: test]
+
+  attr_accessor :compile_tasks
+
+  ##
+  # Initialize variables for compiler plugin.
+
+  def initialize_compiler
+    self.compile_tasks = [:multi, :test]
+    self.spec_extras   = { :extensions => ["ext/#{self.name}/extconf.rb"] }
+
+    extra_dev_deps << ["rake-compiler", "~> 0.7"]
   end
 
   ##
@@ -16,6 +35,10 @@ module Hoe::Compiler
 
     Rake::ExtensionTask.new self.name, spec do |ext|
       ext.lib_dir = File.join(*["lib", self.name, ENV["FAT_DIR"]].compact)
+    end
+
+    compile_tasks.each do |t|
+      task t => :compile
     end
   end
 end
