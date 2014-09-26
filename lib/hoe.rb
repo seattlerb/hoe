50a2706d0f70ece52922ddcc56630e1e0655b83e
Ryan Davis <ryand@zenspider.com>
2014-09-26 14:34:32 -0800

    prepped for release [git-p4: depot-paths = "//src/hoe/dev/": change = 9787]

--- lib/hoe.rb
+++ lib/hoe.rb
@@ -91,7 +91,7 @@ class Hoe
   include Rake::DSL if defined?(Rake::DSL)
 
   # duh
-  VERSION = "3.12.0"
+  VERSION = "3.13.0"
 
   @@plugins = [:clean, :debug, :deps, :flay, :flog, :newb, :package,
                :publish, :gemcutter, :signing, :test]
