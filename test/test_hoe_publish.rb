00f39dc2b55e9bdb0e56edb012c1f57583f1991d
Ryan Davis <ryand@zenspider.com>
2017-07-07 13:48:56 -0800

    oops. fixed test [git-p4: depot-paths = "//src/hoe/dev/": change = 11371]

--- test/test_hoe_publish.rb
+++ test/test_hoe_publish.rb
@@ -17,7 +17,6 @@ class TestHoePublish < Minitest::Test
   def test_make_rdoc_cmd
     expected = %W[
                 #{Gem.ruby}
-                #{nil}
                 #{Gem.bin_wrapper "rdoc"}
                 --title blah-1.0\ Documentation
                 -o doc
