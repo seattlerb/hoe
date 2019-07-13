82a31913a51dff409d00152ab33f578d0d2dd366
Ryan Davis <ryand@zenspider.com>
2019-07-12 18:04:56 -0800

    - Fixed one use of Array.prepend on ruby 2.3-2.4. [git-p4: depot-paths = "//src/hoe/dev/": change = 12181]

--- lib/minitest/test_task.rb
+++ lib/minitest/test_task.rb
@@ -137,7 +137,7 @@ module Minitest # :nodoc:
         ENV["N"] && ENV["N"].to_i > 0
 
       lib_extras = (ENV["MT_LIB_EXTRAS"] || "").split File::PATH_SEPARATOR
-      self.libs.prepend lib_extras
+      self.libs[0,0] = lib_extras
 
       extra_args << "-n" << ENV["N"]                      if ENV["N"]
       extra_args << "-e" << ENV["X"]                      if ENV["X"]
