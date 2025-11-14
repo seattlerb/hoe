faede2926f26c117a74a365d2cbfbdc2c1467fa8
William T. Nelson <35801+wtn@users.noreply.github.com>
2025-11-14 15:00:48 -0600

    Drop Ruby 1.8 compatibility code from `File.read_utf`.

--- lib/hoe.rb
+++ lib/hoe.rb
@@ -953,15 +953,8 @@ end
 class File # :nodoc:
   # Like File::read, but strips out a BOM marker if it exists.
   def self.read_utf path
-    r19 = "<3".respond_to? :encoding
-    opt = r19 ? "r:bom|utf-8" : "rb"
-
-    open path, opt do |f|
-      if r19 then
-        f.read
-      else
-        f.read.sub %r%\A\xEF\xBB\xBF%, ""
-      end
+    open path, "r:bom|utf-8" do |f|
+      f.read
     end
   end
 end
